377c0bb3a4df90090cd24420ff3fc3437cf16310
ppranitharedd <policepatelpranitha03@gmail.com>
2024-03-04 18:14:09 -0600

    Added code for Main Activity (pranitha_reddy)
    Given onclick functionality for the Login and SignUp

--- RecipeAndroidApp/app/src/main/java/com/example/recipeandroidapp/MainActivity.java
+++ RecipeAndroidApp/app/src/main/java/com/example/recipeandroidapp/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.recipeandroidapp;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+
+
+import android.content.Intent;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openLoginPage(View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    public void openSignupPage(View view) {
+        Intent intent = new Intent(this, SignupActivity.class);
+        startActivity(intent);
     }
 }
