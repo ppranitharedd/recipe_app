97a02750defc833b77354bda4681b930357e71ed
Santhosh Reddy Gurram <santhoshreddy4148@gmail.com>
2024-03-04 18:41:09 -0600

    Added Menu Page activity and UI in layout (santhosh_reddy)

---
+++ RecipeAndroidApp/app/src/main/java/com/example/recipeandroidapp/Menu.java
@@ -0,0 +1,48 @@
+package com.example.recipeandroidapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+public class Menu extends AppCompatActivity {
+    private Button btnDesserts, btnMeals, btnBreakfasts, btnDinner;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu);
+        btnDesserts = findViewById(R.id.btnDesserts);
+        btnMeals = findViewById(R.id.btnMeals);
+        btnBreakfasts = findViewById(R.id.btnBreakfasts);
+        btnDinner = findViewById(R.id.btnDinner);
+// Set click listeners for your buttons
+        btnDesserts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Menu.this,DessertsCategory.class);
+                startActivity(intent);
+// Implement your logic for Desserts button click
+// For example, start a new activity or perform some action related to Desserts
+            }
+        });
+        btnMeals.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+// Implement your logic for Meals button click
+            }
+        });
+        btnBreakfasts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+// Implement your logic for Breakfasts button click
+
+            }
+        });
+        btnDinner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+// Implement your logic for Dinner button click
+            }
+        });
+    }
+}
